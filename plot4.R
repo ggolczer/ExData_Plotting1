eeaa16d261701cd7af2974d03e784cd5cb603f76
Gabriel Golczer <g.golczer@gmail.com>
2017-07-20 13:15:06 -0400

    Add files via upload

--- plot4.R
+++ plot4.R
@@ -27,12 +27,29 @@ household_power_lite$Date <- as.Date(household_power_lite$Date, format="%d/%m/%Y
 Date_Time <- paste(household_power_lite$Date, household_power_lite$Time)
 household_power_lite$Date_Time <- as.POSIXct(Date_Time)
 
-#Making Plot 3
+#Making Plot 4
+
+#Create 4 panels
+par(mfcol=c(2,2))
+
+#1st plot
+plot(as.numeric(household_power_lite$Global_active_power)~household_power_lite$Date_Time, 
+type="l", ylab="Global Active Power",xlab=NA)
+
+#2nd plot
+
 plot(x=household_power_lite$Date_Time, y=as.numeric(household_power_lite$Sub_metering_1),type="l",
 ylab="Energy sub metering",xlab=NA,col="black")
 lines(household_power_lite$Date_Time, as.numeric(household_power_lite$Sub_metering_2), col="red")
 lines(household_power_lite$Date_Time, as.numeric(household_power_lite$Sub_metering_3), col="blue")
 legend("topright", lty=1, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 
-dev.copy(png, file="plot3.png", height=480, width=480)
+#3rd plot
+plot(as.numeric(household_power_lite$Voltage)~household_power_lite$Date_Time, 
+type="l", ylab="Voltage",xlab="datetime")
+#4th plot
+plot(as.numeric(household_power_lite$Global_reactive_power)~household_power_lite$Date_Time, 
+type="l", ylab="Global_reactive_power",xlab="datetime")
+
+dev.copy(png, file="plot4.png", height=480, width=480)
 dev.off()
